9a85b487d7a803e356fd0e4950849a2a22261cd5
calvintran1478 <67614346+calvintran1478@users.noreply.github.com>
2023-02-19 23:08:49 +0000

    Build: (294ab55) Update publish.yml to run on Ubuntu

--- src/data/timeline.js
+++
@@ -1,9 +0,0 @@
-export default [
-    {
-        year: 'Today',
-        title: '3rd Year Computer Science Student',
-        duration: '3 years',
-        details:
-            'Lots of detailed information'
-    }
-]
