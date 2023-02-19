edd6d32a3c3692bed79d24892b7b94fa5855404b
Calvin Tran <calvin.tran@mail.utoronto.ca>
2023-02-19 14:39:26 -0500

    Initial commit

---
+++ src/data/portfolio.js
@@ -0,0 +1,20 @@
+export default [
+    {
+        title: 'Nim',
+        imgUrl: '/assets/mountains.jpeg',
+        stack: ['C++'],
+        link: 'https://github.com/calvintran1478/Nim',
+    },
+    {
+        title: 'Nim',
+        imgUrl: '/assets/mountains.jpeg',
+        stack: ['C++'],
+        link: 'https://github.com/calvintran1478/Nim',
+    },
+    {
+        title: 'Nim',
+        imgUrl: '/assets/mountains.jpeg',
+        stack: ['C++'],
+        link: 'https://github.com/calvintran1478/Nim',
+    }
+]
---
+++ src/data/timeline.js
@@ -0,0 +1,9 @@
+export default [
+    {
+        year: 'Today',
+        title: '3rd Year Computer Science Student',
+        duration: '3 years',
+        details:
+            'Lots of detailed information'
+    }
+]
