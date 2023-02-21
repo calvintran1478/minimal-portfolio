c148fe4bd445d0297b0b4cf5fa2c751ae92e0bdc
Calvin Tran <calvin.tran@mail.utoronto.ca>
2023-02-21 02:19:57 -0500

    Add bio

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -8,21 +8,13 @@ function Intro() {
         alt="profile picture"
         className="w-250 h-250 mb-6 md:h-48 rounded object-cover"
       />
-      <h1 className="text-4xl md:text-6xl dark:text-white mb-1 md:mb-3 font-bold">Calvin Tran</h1>
-      <p className="text-base md:text-xl mb-3 font-medium">Computer Science Student and Software Engineer</p>
-      <p className="text-sm max-w-xl mb-6 font-bold">
-        This is my entire bio.{' '}
-        <a
-          href="https://github.com/calvintran1478"
-          target="_blank"
-          className="text-cyan-600 hover:underline underline-offset-2 decoration-2 decoration-cyan-600"
-          rel="noreferrer noopener"
-        >
-          github
-        </a>{' '}
-        More information.
+      <h1 className="text-4xl md:text-5xl dark:text-white mb-1 md:mb-3 font-bold">Calvin Tran</h1>
+      <p className="text-base md:text-xl mb-3 font-semibold">Computer Science Student and Software Engineer</p>
+      <p className="text-base max-w-4xl mb-6 font-medium">
+        Hey I'm Calvin! I'm passionate about all things STEM and combining my knowledge in computer science, physics, and math to solve
+        real world problems. Here you will find some of my personal projects in learning new languages and technologies as well as exploring
+        ideas that interest me.
       </p>
-      Intro
     </div>
   );
 }
